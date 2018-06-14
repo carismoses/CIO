0df895b5c37f3773e96c81d1600d28ea2a5be368
Caris Moses <carism@mit.edu>
2018-06-14 13:54:06 -0400

    Changing param names to match new param handling, returning more things from call to CIO

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import pdb
 from world import Line, Rectangle
 from CIO import CIO
+import params as p
+from util import *
 
 testing1 = False
 testing2 = False
@@ -10,7 +12,7 @@ testing2 = False
 def init_vars(objects):
     _, box, _, _ = objects
     # create the initial system state: s0
-    s0 = np.zeros(len_s)
+    s0 = np.zeros(p.len_s)
 
     # fill in object poses and velocities
     for object in objects:
@@ -29,12 +31,12 @@ def init_vars(objects):
     con1 = [0.0, 0.0, 10.0, 15.0, 0.0] # gripper2
     con2 = [0.0, 0.0,  5.0, 0.0, 1.0] # ground
 
-    s0[18:len_s] = (con0 + con1 + con2)
+    s0[18:p.len_s] = (con0 + con1 + con2)
 
     # initialize traj to all be the same as the starting state
-    S0 = np.zeros(len_S)
-    for k in range(K):
-        S0[k*len_s:k*len_s+len_s] = s0
+    S0 = np.zeros(p.len_S)
+    for k in range(p.K):
+        S0[k*p.len_s:k*p.len_s+p.len_s] = s0
     return s0, S0
 
 #### PARAMETERS #### TODO move them here
@@ -58,49 +60,58 @@ def init_objects():
     return goal, objects
 
 #### test trajectories ####
-from util import *
-
-def make_test_traj(goal, objects):
-    s0, S0 = init_vars(objects)
+def make_test_traj(s0, S0, goal, objects):
     l,r = get_object_pos_ind()
     vl,vr = get_object_vel_ind()
     init_pos = get_object_pos(s0)
     goal = goal[1]
-    interp_poses_x = np.linspace(init_pos[0],goal[0],K+1)
-    interp_poses_y = np.linspace(init_pos[1],goal[1],K+1)
-    interp_poses_th = np.linspace(init_pos[2],goal[2],K+1)
-    for t in range(1,K+1):
-        S0[(t-1)*len_s+l:(t-1)*len_s+r] = [interp_poses_x[t], interp_poses_y[t], \
+    interp_poses_x = np.linspace(init_pos[0],goal[0],p.K+1)
+    interp_poses_y = np.linspace(init_pos[1],goal[1],p.K+1)
+    interp_poses_th = np.linspace(init_pos[2],goal[2],p.K+1)
+    for t in range(1,p.K+1):
+        S0[(t-1)*p.len_s+l:(t-1)*p.len_s+r] = [interp_poses_x[t], interp_poses_y[t], \
                                             interp_poses_th[t]]
         vx = calc_deriv(interp_poses_x[t], interp_poses_x[t-1], delT_phase)
         vy = calc_deriv(interp_poses_y[t], interp_poses_y[t-1], delT_phase)
         vth = calc_deriv(interp_poses_th[t], interp_poses_th[t-1], delT_phase)
-        S0[(t-1)*len_s+vl:(t-1)*len_s+vr] = [vx, vy, vth]
+        S0[(t-1)*p.len_s+vl:(t-1)*p.len_s+vr] = [vx, vy, vth]
     return s0, S0
 
-
-def make_test_traj2(goal, objects):
-    s0, S0 = init_vars(objects)
+def make_test_traj2(s0, S0, goal, objects):
     l,r = get_object_pos_ind()
     vl,vr = get_object_vel_ind()
     init_pos = get_object_pos(s0)
     goal = goal[1]
-    for t in range(1,K+1):
-        if t == K:
-            S0[(t-1)*len_s+l:(t-1)*len_s+r] = goal
+    for t in range(1,p.K+1):
+        if t == p.K:
+            S0[(t-1)*p.len_s+l:(t-1)*len_s+r] = goal
     return s0, S0
 
 #### MAIN FUNCTION ####
-def main():
-    pdb.set_trace()
+def main(test_params):
+    #pdb.set_trace()
+
+    # initialize objects
     goal, objects = init_objects()
+    num_moveable_objects = len(objects) - 1 # don't count the ground
+
+    # set params
+    paramClass = p.Params(test_params, num_moveable_objects)
+    p.set_global_params(paramClass)
+
+    # initialize decision variables
+    s0, S0 = init_vars(objects)
+
+    # potentially update with a test trajcetory
     if testing1:
-        s0, S0 = make_test_traj(goal, objects)
+        s0, S0 = make_test_traj(s0, S0, goal, objects)
     if testing2:
-        s0, S0 = make_test_traj2(goal, objects)
-    else:
-        s0, S0 = init_vars(objects)
-    x = CIO(goal, objects, s0, S0)
+        s0, S0 = make_test_traj2(s0, S0, goal, objects)
+
+    # run CIO
+    x, nit, final_cost = CIO(goal, objects, s0, S0)
+
+    return s0, S0, x, final_cost, nit
 
 if __name__ == '__main__':
     main()
