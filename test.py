16f770b28c6cd3e64c2c0a2321e36df32d32831e
Caris Moses <carism@mit.edu>
2019-05-02 20:34:12 -0400

    removing old files

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ import sys
 if sys.version_info[0] < 3:
     raise Exception("Must be using Python 3")
 
+import tkinter
 import shutil
 import tempfile
 import imageio
 import scipy
 import jupyter
+import matplotlib
+
+print('All dependencies met!')
